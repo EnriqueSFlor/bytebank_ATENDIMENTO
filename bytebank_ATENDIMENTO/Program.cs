5ac1b60f3592e1f0f0892b5bf6eef97dc0ba9cb2
DESKTOP-747UBGO\Enrique <enriquee.flor@gmail.com>
2024-08-21 23:29:22 -0300

    21082024

--- bytebank_ATENDIMENTO/Program.cs
+++ bytebank_ATENDIMENTO/Program.cs
@@ -1,21 +1,71 @@
 ﻿
-
 using bytebank.Modelos.Conta;
+using bytebank_ATENDIMENTO.bytebank.Util;
+using System.Collections;
 
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+#region Exemplos Arrays em C#
 void TestaArrayDeContasCorrentes()
 {
-    ContaCorrente[] listaDeContas = new ContaCorrente[]
-    {
-        new ContaCorrente(874, "5679787-A"),
-        new ContaCorrente(874, "4456668-B"),
-        new ContaCorrente(874, "7781438-C")
-    };
 
-    for (int i = 0; i < listaDeContas.Length; i++)
+    ListaDeContasCorrentes listaDeContas = new ListaDeContasCorrentes();
+    listaDeContas.Adicionar(new ContaCorrente(874, "5679787-A"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "4456668-B"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    listaDeContas.Adicionar(new ContaCorrente(874, "7781438-C"));
+    var contaDoAndre = new ContaCorrente(963, "123456-X");
+    listaDeContas.Adicionar(contaDoAndre);
+    //listaDeContas.ExibeLista();
+    //Console.WriteLine("===============");
+    //listaDeContas.Remover(contaDoAndre);
+    //listaDeContas.ExibeLista();
+
+
+    for (int i = 0; i < listaDeContas.Tamanho; i++)
     {
-        ContaCorrente contaAtual = listaDeContas[i];
-        Console.WriteLine($"Indice[{i}] - Conta {contaAtual.Conta}");
+        //ContaCorrente conta = listaDeContas.RecuperarContaNoIndice(i);
+        ContaCorrente conta = listaDeContas[i];
+        Console.WriteLine($"Indice [{i}] = {conta.Conta}/{conta.Numero_agencia}");
+    }
+
+}
+
+//TestaArrayDeContasCorrentes();
+#endregion
+
+ArrayList _listaDeContas = new ArrayList();
+
+void AtendimentoCliente()
+{
+    char opcao = '0';
+    while (opcao != '6')
+    {
+        Console.Clear();
+        Console.WriteLine("===============================");
+        Console.WriteLine("===       Atendimento       ===");
+        Console.WriteLine("===1 - Cadastrar Conta      ===");
+        Console.WriteLine("===2 - Listar Contas        ===");
+        Console.WriteLine("===3 - Remover Conta        ===");
+        Console.WriteLine("===4 - Ordenar Contas       ===");
+        Console.WriteLine("===5 - Pesquisar Conta      ===");
+        Console.WriteLine("===6 - Sair do Sistema      ===");
+        Console.WriteLine("===============================");
+        Console.WriteLine("\n\n");
+        Console.Write("Digite a opção desejada: ");
+        opcao = Console.ReadLine()[0];
+        switch (opcao)
+        {
+            case '1':
+                //CadastrarConta();
+                break;
+            default:
+                Console.WriteLine("Opcao não implementada.");
+                break;
+        }
     }
 }
 
-TestaArrayDeContasCorrentes();
+AtendimentoCliente();
